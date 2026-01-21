5a867c14e14b9a229d778e9f5d2d5580c6c6a4d5
shahr <shahr@VAFADARMSI>
2026-01-21 22:16:44 +0100

    Add project files.

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            await using (var connection = await factory.CreateConnectionAsync())
+            await using (var channel = await connection.CreateChannelAsync())
+            {
+                await channel.QueueDeclareAsync(queue: "BasicTest",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                var consumer = new AsyncEventingBasicConsumer(channel);
+                consumer.ReceivedAsync += async (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+                    await Task.Yield();
+                };
+
+                await channel.BasicConsumeAsync(queue: "BasicTest",
+                                     autoAck: true,
+                                     consumer: consumer);
+                Console.WriteLine(" [*] Waiting for messages.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+using System;
+using System.Threading.Tasks;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static async Task Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            await using (var connection = await factory.CreateConnectionAsync())
+            await using (var channel = await connection.CreateChannelAsync())
+            {
+                await channel.QueueDeclareAsync(queue: "BasicTest",
+                                durable: false,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                await channel.BasicPublishAsync(
+                    exchange: string.Empty,
+                    routingKey: "BasicTest",
+                    body: body
+                );
+
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit the Sender App.");
+            Console.ReadLine();
+        }
+    }
+}
