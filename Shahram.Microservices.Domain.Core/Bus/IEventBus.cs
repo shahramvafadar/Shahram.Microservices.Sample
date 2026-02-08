ca8f716b77bd410f26b7ba9650917a83646a9a1b
shahramvafadar <shahramvafadar@gmail.com>
2026-02-08 20:57:00 +0100

    1. ver

---
+++ Shahram.Microservices.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,17 @@
+﻿using Shahram.Microservices.Domain.Core.Commands;
+using Shahram.Microservices.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace Shahram.Microservices.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+
+        void Publish<T>(T @event) where T : Event;
+
+        void Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
---
+++ Shahram.Microservices.Domain.Core/Bus/IEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Shahram.Microservices.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shahram.Microservices.Domain.Core.Bus
+{
+    public interface IEventHandler<in TEvent> : IEventHandler
+        where TEvent : Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEventHandler
+    {
+    }
+}
